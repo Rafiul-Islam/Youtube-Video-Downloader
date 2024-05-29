17edc134d42e8b2b8d0279d0424cc328cf806dd1
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-05-29 21:36:36 +0600

    change the server project version, add git ignore file.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.send("Good Luck!");
+  response.send("Hello World!");
 });
 
 app.post("/download", async (req, res) => {
