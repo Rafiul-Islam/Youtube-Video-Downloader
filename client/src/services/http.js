a8b5af98f0cf2af9b95a6c68b0edd4510ae46570
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-05-29 22:48:14 +0600

    update headers for all client request with axios.

--- client/src/services/http.js
+++ client/src/services/http.js
@@ -3,6 +3,11 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 const http = axios.create({
   baseURL: "https://youtube-video-downloader-application-api.vercel.app",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+  },
 });
 
 export default http;
